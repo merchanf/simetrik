73b6a237c5d241d53c7f51d10f2df2277ee13b18
Fabian Merchan <merchanf@javeriana.edu.co>
2020-06-07 16:23:54 -0500

    finished board component

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,14 +1,55 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Select from "./Select/Select";
+import Draggable from "./Draggable/Draggable"
+import { DndProvider } from "react-dnd"
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import './Board.scss';
 
 const Board = ({data}) => {
+  const [sortables, setSortables] = useState([]);
+
+  useEffect(() => {
+
+  });
+
+  const onSelectFirst = (values) => {
+    console.log(values);
+  }
+
+  const onSelectSecond = (values) => {
+    setSortables(values);
+  }
+
+  const onSortChange = (value) => {
+    console.log(value);
+  }
 
   return(
-    <section>
-      <Select data={data} title="¿Qué columnas se repiten?"/>
+    <section id="Board">
+      <div className="column-1">
+        <h1>¿Qué columnas se repiten?</h1>
+        <Select data={data} onSelected={onSelectFirst}/>
+      </div>
+      <div className="column-2">
+        <h1>¿Cómo quieres ordenarlos?</h1>
+        <div className="sort">
+          <DndProvider backend={HTML5Backend}>
+            <Draggable data={sortables} onChange={onSortChange}/>
+          </DndProvider>
+        </div>
+        <div className="select-2">
+          <Select data={data} inverse onSelected={onSelectSecond}/> 
+        </div>
+        <div className="buttons">
+          <button className="cancelar">Cancelar</button>
+          <button className="ok">ok</button>
+        </div>
+      </div>
+      
     </section>
   );
 }
 
 export default Board;
+
+//<Select data={data} title="¿Qué columnas se repiten?"/>
